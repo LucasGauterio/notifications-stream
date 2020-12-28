b1e5426ac9e23297e0d9bf4d96a73243d0321fa9
lucas.gauterio <lucasgauterio@cvccorp.com.br>
2020-12-28 09:56:32 -0300

    change status headers

--- notifications-channel-email/src/main/java/com/cvccorp/notifications/notifications/channel/email/service/NotificationTopicConsumer.java
+++ notifications-channel-email/src/main/java/com/cvccorp/notifications/notifications/channel/email/service/NotificationTopicConsumer.java
@@ -19,7 +19,7 @@ public class NotificationTopicConsumer {
 
     private final EmailService service;
 
-    @StreamListener(target = Sink.INPUT, condition="headers['type']=='template' AND headers['channel']=='email'")
+    @StreamListener(target = Sink.INPUT, condition="headers['status']=='templated' and headers['type']=='email'")
     public void handle(@Payload String message, @Headers MessageHeaders headers) {
         log.info("Received message {} headers {}", message, headers);
         service.process(headers.get(KafkaHeaders.RECEIVED_MESSAGE_KEY).toString(), message);
